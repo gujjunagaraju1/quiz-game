9460327cb8a12fe005d459a06bbe73a3fc7ca6b7
gujjunagaraju1 <gujjunagaraju19@gmail.com>
2023-10-06 13:20:28 +0530

    ignore

--- quiz/settings.py
+++ quiz/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-p2@1&wna3roz=rl%jurms^dv&dk7c6ff!de!fi$f-5*inrd$82'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
